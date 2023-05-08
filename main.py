594896656f91e35e73754683d7e94de7699ef96d
Bartek <bartex600700@gmail.com>
2023-05-08 23:05:27 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("Sieam")
-#Komentarz
-print("zmiana taka")
+from turtle import Screen, Turtle
+
+
+screen = Screen()
+screen.title("Pong Game")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+
+
+
+
+
+screen.exitonclick()
