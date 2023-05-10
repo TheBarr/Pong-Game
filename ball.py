1940f1cda7f1df73a97296f65eddb70c7b26f069
Bartek <bartex600700@gmail.com>
2023-05-11 01:44:23 +0200

    Initial Commit

--- ball.py
+++ ball.py
@@ -6,11 +6,17 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
+        self.x_move = 15
+        self.y_move = 15
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.home()
+        self.bounce_x()
